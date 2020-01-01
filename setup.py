713bdd899d60b3fa125ac85a110df36572fc5a04
Ali Husnain Arshad <banti6350@gmail.com>
2020-01-01 14:07:00 +0500

    1.3

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='freessl',
-	version='1.1',
+	version='1.3',
 	description='Install Free Let\s Encrypt SSL on RunCloud servers in the easy way.',
 	author="Ali Husnain",
 	author_email="info@alihusnain.ml",
 	url="https://github.com/alihusnainarshad/freessl",
-        download_url = 'https://github.com/alihusnainarshad/freessl/archive/1.1.tar.gz',  
+    download_url = 'https://github.com/alihusnainarshad/freessl/archive/1.3.tar.gz',  
 	license="MIT",
 	entry_points={
 		'console_scripts': [
